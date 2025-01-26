818eff88e96b1a28713ed7b84caa29888d695cb0
Utkarsh <159009734+utkarsh-portfolio@users.noreply.github.com>
2025-01-25 22:30:59 -0700

    Add files via upload

---
+++ tools.py
@@ -0,0 +1,11 @@
+# https://serper.dev/
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+os.environ['SERPER_API_KEY'] = os.getenv('SERPER_API_KEY')
+
+from crewai_tools import SerperDevTool
+#initialize the tool for internet searching capabilities
+tool = SerperDevTool()
